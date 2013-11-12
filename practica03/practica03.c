c594cc8fb42d4ebdbd2fdde855a8f4310d68cabb
aguilarcarlos <carlosaguilarnet@gmail.com>
2013-11-12 15:05:54 -0600

    Distancia recorrida con el encoder, pendiente la velocidad cm/s

--- practica03/practica03.c
+++ practica03/practica03.c
@@ -2,18 +2,19 @@
 #fuses XT,NOWDT, PUT, NOWRT
 #use delay(clock=4000000)
 #include <lcd.c>
-Int32 pulsos;
+#include <STDIO.H>
+int32 pulsos;
 float distancia;
 
 #use fast_io(B)
 #INT_EXT
-        
+
+//Interrupción externa        
 ext_isr()
 {
-    if ((input(PIN_B0)==1)&(input(PIN_B1)==0))
+	if ((input(PIN_B0)==1)&(input(PIN_B1)==0))
     {
-    	pulsos = pulsos+1;
-    	distancia = (pulsos / 360) * 5;
+    	pulsos = pulsos+1;	
     }
     
     if ((input(PIN_B0)==1)&(input(PIN_B1)==1))
@@ -21,14 +22,20 @@ ext_isr()
     	pulsos = pulsos-1;
     }
     
+	distancia = pulsos * 0.65;
+	
     lcd_gotoxy(1,1);
     printf(lcd_putc,"Pulsos=%Ld \n",pulsos); 
-    printf(lcd_putc,"Distancia=%Ld",distancia);
+    printf(lcd_putc,"Dist=%Lf cm",distancia);
 }
     
 void main() {
+   //Inicializo la variable pulsos en el codigo principal
    pulsos = 0;
+   
+   // Activar puerto B como entrada
    set_tris_B(0x01); 
+   
    port_b_pullups(TRUE);
    enable_interrupts(int_ext);
    ext_int_edge(L_to_H);
